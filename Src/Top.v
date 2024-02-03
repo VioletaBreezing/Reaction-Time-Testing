273e04e1b070749fd766e940aee4b11093d6538c
Violeta <violet2021@foxmail.com>
2024-02-03 12:21:55 +0800

    改进

--- Src/Top.v
+++ Src/Top.v
@@ -11,30 +11,36 @@ module Top (
     assign rstn    = swi[3];      // 系统复位
 
     // 状态机参数
-    parameter IDLE    = 3'd0;
-    parameter WAIT    = 3'd1;
-    parameter START   = 3'd2;
-    parameter STORAGE = 3'd3;
-    parameter AVERAGE = 3'd4;
-    parameter COMPARE = 3'd5;
+    parameter IDLE     = 3'd0;
+    parameter WAIT     = 3'd1;
+    parameter CLR_CNT1 = 3'd2;
+    parameter START    = 3'd3;
+    parameter STORAGE  = 3'd4;
+    parameter CLR_CNT2 = 3'd5;
+    parameter AVERAGE  = 3'd6;
+    parameter COMPARE  = 3'd7;
     parameter PLAYER_A = 1'b1;
     parameter PLAYER_B = 1'b0;
 
     // 状态机输入信号
-    wire   signal_action, signal_react, signal_average, signal_compare, signal_start, signal_overflow;
+    wire   signal_action, signal_react, signal_average, signal_compare;   // 4 个按键：启动、反应、平均、比较
+    wire   signal_start,  signal_overflow, signal_cleared;                // start: 随机延时结束，开始测量计时
+                                                                          // overflow: 测量超时溢出
+                                                                          // cleared: 计时器清零完成
+
     assign signal_action  = ~key[0];
     assign signal_react   = ~key[1];
     assign signal_average = ~key[2];
     assign signal_compare = ~key[3];
 
     wire   cur_player;
-    assign cur_player = swi[0];
+    assign cur_player = swi[0];         // 切换队友
 
     // 状态机变量
-    reg  [2:0]  machine_state;           // 状态寄存器
-    reg  [12:0] sum_react_time [1:0];    // 累加时间寄存器
-    reg  [2:0]  test_turn      [1:0];    // 测试轮次寄存器
-    wire [15:0] react_time;              // 单次测量时间
+    reg  [2:0]  machine_state;          // 状态寄存器
+    reg  [12:0] sum_react_time [1:0];   // 累加时间寄存器
+    reg  [2:0]  test_turn      [1:0];   // 测试轮次寄存器
+    wire [15:0] react_time;             // 单次测量时间
 
     // 状态转移逻辑
     always @(posedge sys_clk or negedge rstn) begin
@@ -51,10 +57,13 @@ module Top (
                 machine_state <= signal_action ? WAIT : machine_state;
             end
             WAIT   : begin
-                machine_state <= signal_start ? START : machine_state;
+                machine_state <= signal_start ? CLR_CNT1 : machine_state;
+            end
+            CLR_CNT1: begin
+                machine_state <= signal_cleared ? START : machine_state;
             end
             START  : begin
-                if (signal_react || signal_overflow) begin  // signal_react 由按键发出，signal_overflow 由毫秒计时器发出
+                if (signal_react || signal_overflow) begin
                     machine_state <= STORAGE;
                     sum_react_time[cur_player] <= sum_react_time[cur_player] + react_time[12:0];
                 end
@@ -64,10 +73,13 @@ module Top (
                 if (test_turn[cur_player] == 3'd7 && signal_average)
                     machine_state <= AVERAGE;
                 else if (test_turn[cur_player] != 3'd7 && signal_action) begin
-                    machine_state <= WAIT;
+                    machine_state <= CLR_CNT2;
                     test_turn[cur_player] <= test_turn[cur_player] + 3'd1;
                 end
                 else machine_state <= machine_state;
+            end
+            CLR_CNT2: begin
+                machine_state <= signal_cleared ? WAIT : machine_state;
             end
             AVERAGE: begin
                 if (test_turn[PLAYER_A] == 3'd7 && test_turn[PLAYER_B] == 3'd7 && signal_compare)
@@ -116,31 +128,29 @@ module Top (
         .rand   (rand_num_raw)
     );
 
-    // 根据随机数进行延时模块
+    // 计数器，既作延时，也作测量
+    wire [15:0] timer_count;
+    wire        timer_enable, timer_clear;
+
     wire [15:0] delay_count;
-    wire        delay_enable, delay_clear;
-    assign signal_start = (delay_count == rand_num);
-    assign delay_enable = (machine_state == WAIT) && (!signal_start);
-    assign delay_clear  = (machine_state != WAIT);
-    Counter u0_Counter(
-    	.clk    (clk_1KHz    ),
-        .rstn   (rstn        ),
-        .enable (delay_enable),
-        .clear  (delay_clear ),
-        .count  (delay_count )
-    );
-    
-    // 反应时间计时器
-    wire count_enable, count_clear;
-    assign signal_overflow = (react_time == 16'd999);       // 测量反应时间上限定为 1s
-    assign count_enable    = (machine_state == START) && (!signal_overflow);
-    assign count_clear     = (machine_state != START) && (machine_state != STORAGE);
-    Counter u1_Counter(
-    	.clk    (clk_1KHz    ),
-        .rstn   (rstn        ),
-        .enable (count_enable),
-        .clear  (count_clear ),
-        .count  (react_time  )
+
+    assign react_time  = timer_count;
+    assign delay_count = timer_count;
+
+    assign signal_start    = (delay_count == rand_num);
+    assign signal_overflow = (react_time  == 16'd999);
+    assign signal_cleared  = (timer_count == 16'd0);
+
+    assign timer_enable = ((machine_state == WAIT)  && (!signal_start)) ||
+                          ((machine_state == START) && (!signal_overflow));
+    assign timer_clear  = (machine_state == CLR_CNT1) || (machine_state == CLR_CNT2);
+
+    Counter u_Timer(
+    	.clk       (clk_1KHz    ),
+        .rstn      (rstn        ),
+        .enable    (timer_enable),
+        .clear     (timer_clear ),
+        .count     (timer_count )
     );
 
     // 数码管显示控制
@@ -151,6 +161,10 @@ module Top (
         if (!rstn) begin
             enable_circle_shift <= 1'b0;
             data_display        <= 10'b0;
+        end
+        else if (machine_state == START) begin
+            data_display        <= react_time[9:0];
+            enable_circle_shift <= 1'b0;
         end
         else if (machine_state == STORAGE) begin
             data_display        <= react_time[9:0];
@@ -166,7 +180,7 @@ module Top (
             enable_circle_shift <= 1'b1;
         end
         else begin
-            data_display        <= react_time[9:0];
+            data_display        <= 10'b0;
             enable_circle_shift <= 1'b0;
         end
     end
@@ -202,8 +216,8 @@ module Top (
     parameter WHITE = 3'b111;
     parameter BLACK = 3'b000;
 
-    parameter FULL  = 1'b1;
-    parameter SEMI  = 1'b0;
+    parameter FULL  = 1'b1;     // 亮度：大亮
+    parameter SEMI  = 1'b0;     // 亮度：半亮
 
     reg [2:0] color  [1:0];
     reg [1:0] bright;
@@ -260,7 +274,7 @@ module Top (
         if (!rstn)
             led <= {8{OFF}};
         else if (machine_state == START || machine_state == STORAGE)
-            led[test_turn[cur_player]] <= 1'b0;
+            led[test_turn[cur_player]] <= ON;
         else if (machine_state == AVERAGE)
             led <= (test_turn[cur_player] == 3'd7) ? {8{ON}} : {8{OFF}};
         else
